876022d105b70582de9ea8abd1812a96dc0fb48c
Karol Bulwin <karolbulwin@gmail.com>
2019-03-14 22:16:52 +0100

    custom  form validation

---
+++ public/js/custom-validation.js
@@ -0,0 +1,14 @@
+(function customValidation() {
+  window.addEventListener('load', () => {
+    const forms = document.getElementsByClassName('needs-validation');
+    Array.prototype.filter.call(forms, (form) => {
+      form.addEventListener('submit', (event) => {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+}());
