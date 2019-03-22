6922f63e761f582516b10e0a4b523e66f75800b7
Karol Bulwin <karolbulwin@gmail.com>
2019-03-22 17:49:36 +0100

    for nested objects

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session(
   {
