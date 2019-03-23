521e81497054a778cb1aac11f2afdd88ff7acc9b
Karol Bulwin <karolbulwin@gmail.com>
2019-03-23 09:12:39 +0100

    admin controller

---
+++ src/controllers/adminController.js
@@ -0,0 +1,120 @@
+const mongoose = require('mongoose');
+const debug = require('debug')('app:adminController');
+const chalk = require('chalk');
+const Book = require('../models/bookSchema');
+
+const books = [
+  {
+    title: 'The Hunger Games',
+    genre: 'Fantasy',
+    author: 'Suzanne Collins',
+    isRented: false,
+    isReserved: false,
+    rentedBy: null,
+    reservedBy: null
+  },
+  {
+    title: 'Harry Potter and Order of the Phoenix',
+    genre: 'Fantasy',
+    author: 'J. K. Rowling',
+    isRented: false,
+    isReserved: false,
+    rentedBy: null,
+    reservedBy: null
+  },
+  {
+    title: 'To Kill Mockingbird',
+    genre: 'Classics',
+    author: 'Harper Lee',
+    isRented: false,
+    isReserved: false,
+    rentedBy: null,
+    reservedBy: null
+  },
+  {
+    title: 'Pride and Prejudice',
+    genre: 'Classics',
+    author: 'Jane Austen',
+    isRented: false,
+    isReserved: false,
+    rentedBy: null,
+    reservedBy: null
+  },
+  {
+    title: 'Twilight',
+    genre: 'Fantasy',
+    author: 'Stephenie Meyer',
+    isRented: false,
+    isReserved: false,
+    rentedBy: null,
+    reservedBy: null
+  },
+  {
+    title: 'The Book Thief',
+    genre: 'Historical Fiction',
+    author: 'Mark Zusak',
+    isRented: false,
+    isReserved: false,
+    rentedBy: null,
+    reservedBy: null
+  },
+  {
+    title: 'The Chronicles of Narnia',
+    genre: 'Fantasy',
+    author: 'C. S. Lewis',
+    isRented: false,
+    isReserved: 3,
+    rentedBy: null,
+    reservedBy: null
+  },
+  {
+    title: 'Animal Farm',
+    genre: 'Classics',
+    author: 'George Orwell',
+    isRented: 1,
+    isReserved: 3,
+    rentedBy: null,
+    reservedBy: null
+  }];
+
+function authController(nav) {
+  function middleware(req, res, next) {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect('/');
+    }
+  }
+  function adminPage(req, res) {
+    res.render('adminView', {
+      nav,
+      title: 'Admin'
+    });
+  }
+  function addBooks(req, res) {
+    const url = 'mongodb://localhost:27017/libraryApp';
+    (async function mongo() {
+      try {
+        await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
+        debug(`${chalk.green('Connected correctly to server - add books')}`);
+
+        Book.collection.insertMany(books, (err) => {
+          if (err) {
+            debug(err);
+          }
+          res.json(books);
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      mongoose.disconnect();
+    }());
+  }
+  return {
+    middleware,
+    adminPage,
+    addBooks
+  };
+}
+
+module.exports = authController;
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,114 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const debug = require('debug')('app:adminRoutes');
-const chalk = require('chalk');
-const Book = require('../models/bookSchema');
-
-const books = [
-  {
-    title: 'The Hunger Games',
-    genre: 'Fantasy',
-    author: 'Suzanne Collins',
-    isRented: false,
-    isReserved: false,
-    rentedBy: null,
-    reservedBy: null
-  },
-  {
-    title: 'Harry Potter and Order of the Phoenix',
-    genre: 'Fantasy',
-    author: 'J. K. Rowling',
-    isRented: false,
-    isReserved: false,
-    rentedBy: null,
-    reservedBy: null
-  },
-  {
-    title: 'To Kill Mockingbird',
-    genre: 'Classics',
-    author: 'Harper Lee',
-    isRented: false,
-    isReserved: false,
-    rentedBy: null,
-    reservedBy: null
-  },
-  {
-    title: 'Pride and Prejudice',
-    genre: 'Classics',
-    author: 'Jane Austen',
-    isRented: false,
-    isReserved: false,
-    rentedBy: null,
-    reservedBy: null
-  },
-  {
-    title: 'Twilight',
-    genre: 'Fantasy',
-    author: 'Stephenie Meyer',
-    isRented: false,
-    isReserved: false,
-    rentedBy: null,
-    reservedBy: null
-  },
-  {
-    title: 'The Book Thief',
-    genre: 'Historical Fiction',
-    author: 'Mark Zusak',
-    isRented: false,
-    isReserved: false,
-    rentedBy: null,
-    reservedBy: null
-  },
-  {
-    title: 'The Chronicles of Narnia',
-    genre: 'Fantasy',
-    author: 'C. S. Lewis',
-    isRented: false,
-    isReserved: false,
-    rentedBy: null,
-    reservedBy: null
-  },
-  {
-    title: 'Animal Farm',
-    genre: 'Classics',
-    author: 'George Orwell',
-    isRented: false,
-    isReserved: false,
-    rentedBy: null,
-    reservedBy: null
-  }];
-
-const newBooks = [];
-
-/* use schema for check input */
-books.forEach((book) => {
-  const newBook = new Book(book);
-  newBooks.push(newBook);
-});
+const adminController = require('../controllers/adminController');
 
 const adminRouter = express.Router();
 
-function router() {
+function router(nav) {
+  const {
+    middleware,
+    adminPage,
+    addBooks
+  } = adminController(nav);
+  adminRouter.use(middleware);
   adminRouter.route('/')
-    .get((req, res) => {
-      const url = 'mongodb://localhost:27017/libraryApp';
-      (async function mongo() {
-        try {
-          await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
-          debug(`${chalk.green('Connected correctly to server - add books')}`);
+    .get(adminPage);
+  adminRouter.route('/addBooks')
+    .get(addBooks);
 
-          Book.collection.insertMany(newBooks, (err) => {
-            if (err) {
-              debug(err);
-            }
-            res.json(newBooks);
-          });
-        } catch (err) {
-          debug(err.stack);
-        }
-        mongoose.disconnect();
-      }());
-    });
   return adminRouter;
 }
 
