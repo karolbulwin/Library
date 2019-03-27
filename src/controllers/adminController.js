69fa8ca73fdec83db85fc054e507386cb911dcf8
Karol Bulwin <karolbulwin@gmail.com>
2019-03-27 12:59:05 +0100

    give, cancel, take, edit, delete, add

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const debug = require('debug')('app:adminController');
 const chalk = require('chalk');
 const Book = require('../models/bookSchema');
+const User = require('../models/userSchema');
 
 const booksList = [
   {
@@ -63,7 +64,7 @@ const booksList = [
     genre: 'Fantasy',
     author: 'C. S. Lewis',
     isRented: false,
-    isReserved: 3,
+    isReserved: false,
     rentedBy: null,
     reservedBy: null
   },
@@ -71,8 +72,8 @@ const booksList = [
     title: 'Animal Farm',
     genre: 'Classics',
     author: 'George Orwell',
-    isRented: 1,
-    isReserved: 3,
+    isRented: false,
+    isReserved: false,
     rentedBy: null,
     reservedBy: null
   }];
@@ -98,8 +99,9 @@ function adminController(nav) {
         debug('Connected correctly to server - get all books for admin');
 
         await Book.find({}, (err, books) => {
-          debug(err);
-          debug(books);
+          if (err) {
+            debug(`err: ${err}`);
+          }
 
           res.render('adminView', {
             nav,
@@ -113,7 +115,171 @@ function adminController(nav) {
       mongoose.disconnect();
     }());
   }
+  function giveTheBookToTheUser(req, res) {
+    const url = 'mongodb://localhost:27017/libraryApp';
+    const {
+      _id,
+      reservedBy
+    } = req.body;
 
+    (async function updateUser() {
+      try {
+        await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
+        debug(`${chalk.green('Connected correctly to server - giveTheBookToTheUser - user')}`);
+
+        await User.findOneAndUpdate({ username: reservedBy }, {
+          hasRented: true,
+          hasReserved: false
+        }, (err, user) => {
+          if (err) {
+            debug(err);
+          }
+          debug('2');
+
+          debug(user);
+          debug('2');
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      mongoose.disconnect();
+    }());
+    (async function updateBook() {
+      try {
+        await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
+        debug(`${chalk.green('Connected correctly to server - giveTheBookToTheUser - book')}`);
+
+        await Book.findOneAndUpdate({ _id }, {
+          isRented: true,
+          isReserved: false,
+          rentedBy: reservedBy,
+          reservedBy: null
+        }, (err, s) => {
+          debug('1');
+          if (err) {
+            debug(err);
+          }
+          debug(s);
+          debug('1');
+
+          res.redirect('/admin'); // dont work with mongoose - use reload on client page -- need to change it!
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      mongoose.disconnect();
+    }());
+  }
+  function cancelReservation(req, res) {
+    const url = 'mongodb://localhost:27017/libraryApp';
+    const {
+      _id,
+      reservedBy
+    } = req.body;
+
+    debug(req.body);
+
+    (async function updateBook() {
+      try {
+        await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
+        debug(`${chalk.green('Connected correctly to server - cancelReservation - book')}`);
+
+        await Book.findOneAndUpdate({ _id }, {
+          isReserved: false,
+          reservedBy: null
+        }, (err, book) => {
+          debug('1');
+          if (err) {
+            debug(err);
+          }
+          debug(book);
+          debug('1');
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      mongoose.disconnect();
+    }());
+    (async function updateUser() {
+      try {
+        await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
+        debug(`${chalk.green('Connected correctly to server - cancelReservation - user')}`);
+
+        await User.findOneAndUpdate({ username: reservedBy }, {
+          hasReserved: false,
+          bookId: null
+        }, (err, user) => {
+          if (err) {
+            debug(err);
+          }
+          debug('2');
+
+          debug(user);
+          debug('2');
+
+          res.redirect('/admin'); // dont work with mongoose - use reload on client page -- need to change it!
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      mongoose.disconnect();
+    }());
+  }
+  function takeTheBookFromTheUser(req, res) {
+    const url = 'mongodb://localhost:27017/libraryApp';
+    const {
+      _id
+    } = req.body;
+
+    const {
+      username
+    } = req.user;
+
+    (async function updateBook() {
+      try {
+        await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
+        debug(`${chalk.green('Connected correctly to server - takeBookFromTheUser - book')}`);
+
+        await Book.findOneAndUpdate({ _id }, {
+          isRented: false,
+          rentedBy: null
+        }, (err, book) => {
+          debug('1');
+          if (err) {
+            debug(err);
+          }
+          debug(book);
+          debug('1');
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      mongoose.disconnect();
+    }());
+    (async function updateUser() {
+      try {
+        await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
+        debug(`${chalk.green('Connected correctly to server - giveTheBookToTheUser - user')}`);
+
+        await User.findOneAndUpdate({ username }, {
+          hasRented: false,
+          bookId: null
+        }, (err, user) => {
+          if (err) {
+            debug(err);
+          }
+          debug('2');
+
+          debug(user);
+          debug('2');
+        });
+        res.redirect('/admin'); // dont work with mongoose - use reload on client page -- need to change it!
+      } catch (err) {
+        debug(err.stack);
+      }
+      mongoose.disconnect();
+    }());
+  }
   function addBooks(req, res) {
     const url = 'mongodb://localhost:27017/libraryApp';
     (async function mongo() {
@@ -121,11 +287,106 @@ function adminController(nav) {
         await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
         debug(`${chalk.green('Connected correctly to server - add books')}`);
 
-        Book.collection.insertMany(booksList, (err) => {
+        await Book.collection.insertMany(booksList, (err) => {
           if (err) {
             debug(err);
           }
-          res.json(booksList);
+          res.redirect('/admin'); // dont work with mongoose - use reload on client page -- need to change it!
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      mongoose.disconnect();
+    }());
+  }
+  function addBook(req, res) {
+    const url = 'mongodb://localhost:27017/libraryApp';
+
+    debug(req.body);
+    const {
+      title,
+      author,
+      genre
+    } = req.body;
+
+    (async function mongo() {
+      try {
+        await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
+        debug(`${chalk.green('Connected correctly to server - add book')}`);
+
+        const newBook = new Book({
+          title,
+          author,
+          genre,
+          isReserved: false,
+          isRented: false
+        });
+
+        await newBook.save((err) => {
+          if (err) {
+            debug(err);
+          }
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      setTimeout(() => { // server instance pool was destroy ?? save?
+        mongoose.disconnect();
+      }, 1000);
+    }());
+  }
+  function editBook(req, res) {
+    const url = 'mongodb://localhost:27017/libraryApp';
+    debug(req.body);
+    const {
+      _id,
+      title,
+      author,
+      genre
+    } = req.body;
+    (async function mongo() {
+      try {
+        await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
+        debug(`${chalk.green('Connected correctly to server - edit book')}`);
+
+        await Book.findOneAndUpdate({ _id }, {
+          title,
+          author,
+          genre
+        }, (err, s) => {
+          debug('1');
+          if (err) {
+            debug(err);
+          }
+          debug(s);
+          debug('1');
+
+          res.redirect('/admin'); // dont work with mongoose - use reload on client page -- need to change it!
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      mongoose.disconnect();
+    }());
+  }
+  function deleteBook(req, res) {
+    const url = 'mongodb://localhost:27017/libraryApp';
+
+    const {
+      _id
+    } = req.body;
+
+    (async function mongo() {
+      try {
+        await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
+        debug(`${chalk.green('Connected correctly to server - delete book')}`);
+
+        await Book.findOneAndDelete({ _id }, (err) => {
+          if (err) {
+            debug(err);
+          }
+          debug('book deleted');
+          // res.redirect('/admin'); dont work with mongoose - use reload on client page -- need to change it!
         });
       } catch (err) {
         debug(err.stack);
@@ -136,7 +397,13 @@ function adminController(nav) {
   return {
     middleware,
     adminPage,
-    addBooks
+    giveTheBookToTheUser,
+    cancelReservation,
+    takeTheBookFromTheUser,
+    editBook,
+    deleteBook,
+    addBooks,
+    addBook
   };
 }
 
