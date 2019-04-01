9c092c61474b870dde536c73f4a299bc7295ac52
Karol Bulwin <karolbulwin@gmail.com>
2019-04-01 21:45:46 +0200

    changed for mongoose & ajax

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,47 +1,29 @@
 const passport = require('passport');
 const { Strategy } = require('passport-local');
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 const debug = require('debug')('app:local.strategy');
+const User = require('../../models/userSchema');
 
 function localStrategy() {
-  passport.use(new Strategy(
-    {
-      usernameField: 'username',
-      passwordField: 'password'
-    }, (username, password, done) => {
-      const url = 'mongodb://localhost:27017';
-      const dbName = 'libraryApp';
-      (async function mongo() {
-        let client;
+  passport.use(new Strategy((username, password, done) => {
+    const url = 'mongodb://localhost:27017/libraryApp';
+    (async function mongo() {
+      await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
+      try {
+        debug('Connected correctly to server - Sing in');
 
-        try {
-          client = await MongoClient.connect(url, { useNewUrlParser: true });
-
-          debug('Connected correctly to server - Sing in');
-
-          const db = client.db(dbName);
-          const col = db.collection('users');
-
-          const user = await col.findOne({ username });
-
-          if (user) {
-            if (user.password === password) {
-              done(null, user);
-            } else {
-              done(null, false);
-              debug('Wrong password'); // TODO
-            }
-          } else {
-            done(null, false);
-            debug('User doesnt exist'); // TODO
-          }
-        } catch (err) {
-          debug(err.stack);
-        }
-        client.close();
-      }());
-    }
-  ));
+        await User.findOne({ username }, (err, user) => {
+          if (err) { debug(err); return done(err); }
+          if (!user) { return done(null, false, { message: 'Incorrect username.' }); }
+          if (user.password !== password) { return done(null, false, { message: 'Incorrect password.' }); }
+          return done(null, user);
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      mongoose.disconnect();
+    }());
+  }));
 }
 
 module.exports = localStrategy;
