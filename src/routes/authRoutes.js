8ebca3c2c76cbf5b3d291d5cd1feb1c503d94acf
Karol Bulwin <karolbulwin@gmail.com>
2019-04-01 21:43:53 +0200

    moved to app - couldn't get res to make itWorkHere

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const passport = require('passport');
 const authController = require('../controllers/authController');
 
 const authRouter = express.Router();
@@ -13,11 +12,6 @@ function router(nav) {
   authRouter.route('/signUp')
     .get(signUpView)
     .post(signUp);
-  authRouter.route('/signIn')
-    .post(passport.authenticate('local', {
-      successRedirect: '/books',
-      failureRedirect: '/'
-    }));
   authRouter.route('/logout')
     .get(logout);
   return authRouter;
