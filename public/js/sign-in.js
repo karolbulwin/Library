87bc8f5dd459288ba15a3fb63ff55392e20b8566
Karol Bulwin <karolbulwin@gmail.com>
2019-06-06 11:50:34 +0200

    prevent from submit if form does not match pattern

--- public/js/sign-in.js
+++ public/js/sign-in.js
@@ -1,38 +1,57 @@
-(function signIn() {
-  document.querySelector('form[name=signInForm]').addEventListener('submit', function form(e) {
-    e.preventDefault();
-    const xhr = new XMLHttpRequest();
-    const formData = new FormData(this);
-    const data = {};
+(function customValidation() {
+  window.addEventListener(
+    'load',
+    () => {
+      const forms = document.getElementsByClassName('needs-validation');
+      Array.prototype.filter.call(forms, (form) => {
+        form.addEventListener(
+          'submit',
+          (event) => {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            } else {
+              event.preventDefault();
+              const xhr = new XMLHttpRequest();
+              const formData = new FormData(form);
+              const data = {};
 
-    // eslint-disable-next-line prefer-const
-    for (let input of formData) {
-      data[input[0]] = input[1];
-    }
+              // eslint-disable-next-line prefer-const
+              for (let input of formData) {
+                data[input[0]] = input[1];
+              }
 
-    xhr.onload = () => {
-      const res = JSON.parse(xhr.response);
-      if (res.message === 'Incorrect username.') {
-        document.querySelector('#username').value = '';
-        document.querySelector('#invalid-user').innerText = res.message;
-        document.querySelector('#username').onclick = () => {
-          document.querySelector('#invalid-user').innerText = '';
-        };
-      }
-      if (res.message === 'Incorrect password.') {
-        document.querySelector('#password').value = '';
-        document.querySelector('#invalid-password').innerText = res.message;
-        document.querySelector('#password').onclick = () => {
-          document.querySelector('#invalid-password').innerText = '';
-        };
-      }
-      if (res.message === 'logedIn') {
-        window.location.replace(res.url);
-      }
-    };
+              xhr.onload = () => {
+                const res = JSON.parse(xhr.response);
+                if (res.message === 'Incorrect username.') {
+                  document.querySelector('#username').value = '';
+                  document.querySelector('#invalid-user').innerText = res.message;
+                  document.querySelector('#username').onclick = () => {
+                    document.querySelector('#invalid-user').innerText = '';
+                  };
+                }
+                if (res.message === 'Incorrect password.') {
+                  document.querySelector('#password').value = '';
+                  document.querySelector('#invalid-password').innerText = res.message;
+                  document.querySelector('#password').onclick = () => {
+                    document.querySelector('#invalid-password').innerText = '';
+                  };
+                }
+                if (res.message === 'logedIn') {
+                  window.location.replace(res.url);
+                }
+              };
 
-    xhr.open('POST', '/auth/signIn', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(data));
-  });
+              xhr.open('POST', '/auth/signIn', true);
+              xhr.setRequestHeader('Content-Type', 'application/json');
+              xhr.send(JSON.stringify(data));
+            }
+            form.classList.add('was-validated');
+          },
+          false
+        );
+      });
+    },
+    false
+  );
 }());
